8b921e87e6c2baa0f3d3fb30eb3d265842846fbf
tudorsonycx <tudorsonycx@gmail.com>
2025-01-06 22:59:59 +0000

    fix: rename character set variable for base62 encoding clarity

--- src/short_url.py
+++ src/short_url.py
@@ -1,6 +1,6 @@
 import string
 
-CHARS = string.digits + string.ascii_lowercase + string.ascii_uppercase
+CHARS_62 = string.digits + string.ascii_lowercase + string.ascii_uppercase
 
 
 def encode_uid_62(uid: int) -> str:
@@ -16,6 +16,6 @@ def encode_uid_62(uid: int) -> str:
     encoded_chars = ""
     while uid:
         r = uid % 62
-        encoded_chars = CHARS[r] + encoded_chars
+        encoded_chars = CHARS_62[r] + encoded_chars
         uid //= 62
     return encoded_chars
