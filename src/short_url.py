b92e3d0ba2d4cd84f58f434c55ceb90c869f0af0
tudorsonycx <tudorsonycx@gmail.com>
2025-01-06 22:59:35 +0000

    add docstring to encode_uid_62 function for clarity

--- src/short_url.py
+++ src/short_url.py
@@ -4,6 +4,15 @@ CHARS = string.digits + string.ascii_lowercase + string.ascii_uppercase
 
 
 def encode_uid_62(uid: int) -> str:
+    """
+    Encodes a given integer UID into a base-62 string.
+
+    Args:
+        uid (int): The integer UID to encode.
+
+    Returns:
+        str: The base-62 encoded string representation of the UID.
+    """
     encoded_chars = ""
     while uid:
         r = uid % 62
